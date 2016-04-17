61beaa4d233b76efb72fb229300c60a7054b6faa
xuxiaomeng <xuxiaomeng@baozou.com>
2016-04-17 14:17:33 +0800

    bugfix. fix upload image.

--- root/src/components/images.jsx
+++ root/src/components/images.jsx
@@ -1,35 +1,5 @@
-const pic1 = document.createElement('img');
-pic1.src = require('./../../assets/images/Logo-big-1.png');
-pic1.onload = (v => Images._load('Logo-big-1.png'));
-const pic2 = document.createElement('img');
-pic2.src = require('./../../assets/images/Logo-big-2.png');
-pic2.onload = (v => Images._load('Logo-big-2.png'));
-const pic3 = document.createElement('img');
-pic3.src = require('./../../assets/images/Logo-big-3.png');
-pic3.onload = (v => Images._load('Logo-big-3.png'));
-const pic4 = document.createElement('img');
-pic4.src = require('./../../assets/images/Logo-big-4.png');
-pic4.onload = (v => Images._load('Logo-big-4.png'));
-const pic5 = document.createElement('img');
-pic5.src = require('./../../assets/images/Logo-big-5.png');
-pic5.onload = (v => Images._load('Logo-big-5.png'));
-const pictextlog = document.createElement('img');
-pictextlog.src = require('./../../assets/images/Cleverious-big.png');
-pictextlog.onload = (v => Images._load('Cleverious-big.png'));
-const pichomebg = document.createElement('img');
-pichomebg.src = require('./../../assets/images/home-page-bg.png');
-pichomebg.onload = (v => Images._load('home-page-bg.png'));
-
 const Images = {
-  'Logo-big-1.png': { src: pic1.src, loaded: false},
-  'Logo-big-2.png': { src: pic2.src, loaded: false},
-  'Logo-big-3.png': { src: pic3.src, loaded: false},
-  'Logo-big-4.png': { src: pic4.src, loaded: false},
-  'Logo-big-5.png': { src: pic5.src, loaded: false},
-  'Cleverious-big.png': { src: pictextlog.src, loaded: false},
-  'home-page-bg.png': { src: pichomebg.src, loaded: false},
-
-  names: ['Logo-big-1.png', 'Logo-big-2.png', 'Logo-big-3.png', 'Logo-big-4.png', 'Logo-big-5.png', 'Cleverious-big.png', 'home-page-bg.png'],
+  names: [],
   _load(name) {
     this[name].loaded = true;
     for (var i = 0; i < this._loadcbs.length; i++) {
@@ -54,7 +24,27 @@ const Images = {
     });
     cb();
     this._loadcbs.push(cb);
+  },
+  
+  pushImage(key, src) {
+    var image = new Image();
+    image.src = src;
+    this[key] = {
+      src: image.src,
+      loaded: false
+    };
+    var self = this;
+    image.onload = (v => self._load(key));
+    this.names.push[key];
   }
 }
+
+Images.pushImage('Logo-big-1.png', require('./../../assets/images/Logo-big-1.png'));
+Images.pushImage('Logo-big-2.png', require('./../../assets/images/Logo-big-2.png'));
+Images.pushImage('Logo-big-3.png', require('./../../assets/images/Logo-big-3.png'));
+Images.pushImage('Logo-big-4.png', require('./../../assets/images/Logo-big-4.png'));
+Images.pushImage('Logo-big-5.png', require('./../../assets/images/Logo-big-5.png'));
+Images.pushImage('Cleverious-big.png', require('./../../assets/images/Cleverious-big.png'));
+Images.pushImage('home-page-bg.png', require('./../../assets/images/home-page-bg.png'));
 
 module.exports = Images;
